40304b7d4da0dd1c610c74d1658a10b7b89d6b6f
yusuf <yusuf@DESKTOP-I9NEB11>
2022-06-25 16:02:36 +0300

    Add project files.

---
+++ MVCPartialDevam_2/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using MVCPartialDevam_2.DesignPatterns.SingletonPattern;
+using MVCPartialDevam_2.Models;
+using MVCPartialDevam_2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPartialDevam_2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        NorthwindEntities _db;
+
+
+        List<Product> _products;
+        List<Category> _categories;
+
+        public HomeController()
+        {
+            _db = DBTool.DBInstance;
+            _products = _db.Products.ToList();
+            _categories = _db.Categories.ToList();
+        }
+
+        public ActionResult Index()
+        {
+            HomeVM hvm = new HomeVM
+            {
+                Employees = _db.Employees.ToList()
+            };
+            return View(hvm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public PartialViewResult GetAllCategories()
+        {
+            HomeVM hvm = new HomeVM
+            {
+                Categories = _categories
+               
+               
+             
+            };
+
+            return PartialView("_CategoryPartial", hvm);
+
+           
+        }
+
+        public PartialViewResult GetAllProducts()
+        {
+            HomeVM hvm = new HomeVM
+            {
+                Products = _products
+            };
+            return PartialView("_ProductPartial", hvm);
+        }
+    }
+}
---
+++ MVCPartialDevam_2/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using MVCPartialDevam_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPartialDevam_2.ViewModels
+{
+    public class HomeVM
+    {
+        public Employee Employee { get; set; }
+        public List<Employee> Employees { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
